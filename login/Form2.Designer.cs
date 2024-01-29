b3d2686616f37f1659997438c98dc03bca1efe4a
tharidusathsara200@gmail.com <tharidusathsara200@gmail.com>
2024-01-29 15:22:59 +0530

    sample d

--- login/Form2.Designer.cs
+++ login/Form2.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -40,11 +41,22 @@
             label1.TabIndex = 0;
             label1.Text = "test";
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(166, 253);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 20);
+            label2.TabIndex = 1;
+            label2.Text = "sathsara";
+            label2.Click += label2_Click;
+            // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form2";
             Text = "Form2";
@@ -55,5 +67,6 @@
         #endregion
 
         private Label label1;
+        private Label label2;
     }
 }
--- login/Form2.cs
+++ login/Form2.cs
@@ -16,5 +16,10 @@ namespace login
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
