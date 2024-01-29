5850d588bde970207fa04ac09ccf4d6288bb51c5
tharidusathsara200@gmail.com <tharidusathsara200@gmail.com>
2024-01-29 23:34:22 +0530

    Form 2

--- login/Form3.Designer.cs
+++ login/Form3.Designer.cs
@@ -31,16 +31,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             panel1 = new Panel();
             panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
             pictureBox1 = new PictureBox();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
+            panel5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.BackColor = Color.Blue;
+            panel1.BackColor = Color.LightSkyBlue;
             panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Controls.Add(panel5);
+            panel1.Controls.Add(panel3);
             panel1.Controls.Add(panel2);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
@@ -50,22 +55,46 @@
             // 
             // panel2
             // 
-            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
-            panel2.Controls.Add(pictureBox1);
-            panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(0, 0);
+            panel2.Location = new Point(257, 0);
             panel2.Name = "panel2";
-            panel2.Size = new Size(258, 171);
-            panel2.TabIndex = 0;
+            panel2.Size = new Size(942, 156);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackgroundImage = (Image)resources.GetObject("panel3.BackgroundImage");
+            panel3.Location = new Point(257, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(939, 112);
+            panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.LightSkyBlue;
+            panel4.BackgroundImage = (Image)resources.GetObject("panel4.BackgroundImage");
+            panel4.Location = new Point(257, 0);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(942, 92);
+            panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            panel5.BackgroundImage = (Image)resources.GetObject("panel5.BackgroundImage");
+            panel5.Controls.Add(pictureBox1);
+            panel5.Dock = DockStyle.Top;
+            panel5.Location = new Point(0, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(258, 167);
+            panel5.TabIndex = 3;
             // 
             // pictureBox1
             // 
-            pictureBox1.BackColor = Color.RoyalBlue;
             pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Dock = DockStyle.Top;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(43, 12);
+            pictureBox1.Location = new Point(0, 0);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(172, 156);
+            pictureBox1.Size = new Size(258, 92);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -76,6 +105,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1200, 720);
+            Controls.Add(panel4);
             Controls.Add(panel1);
             Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             FormBorderStyle = FormBorderStyle.None;
@@ -85,7 +115,7 @@
             Text = "Form3";
             WindowState = FormWindowState.Minimized;
             panel1.ResumeLayout(false);
-            panel2.ResumeLayout(false);
+            panel5.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
@@ -94,6 +124,9 @@
 
         private Panel panel1;
         private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
         private PictureBox pictureBox1;
     }
 }
