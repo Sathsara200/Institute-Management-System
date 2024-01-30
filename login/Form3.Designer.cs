371203ec0aa6b2eb0813627d0aa2f361c08eb0c5
tharidusathsara200@gmail.com <tharidusathsara200@gmail.com>
2024-01-30 19:30:46 +0530

    I change form2

--- login/Form3.Designer.cs
+++ login/Form3.Designer.cs
@@ -30,29 +30,34 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             panel1 = new Panel();
+            pictureBox4 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            btnExit = new Button();
+            btnAttendance = new Button();
+            btnDashboard = new Button();
+            btnProfile = new Button();
             panel5 = new Panel();
+            label3 = new Label();
+            label1 = new Label();
             pictureBox1 = new PictureBox();
             panel3 = new Panel();
             panel2 = new Panel();
             panel4 = new Panel();
+            label4 = new Label();
+            label2 = new Label();
             pictureBox2 = new PictureBox();
-            textwelcome = new TextBox();
             textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            btnDashboard = new Button();
-            pictureBox3 = new PictureBox();
-            button2 = new Button();
-            pictureBox4 = new PictureBox();
-            pictureBox5 = new PictureBox();
-            button1 = new Button();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             panel5.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -60,11 +65,13 @@
             panel1.BackColor = Color.Indigo;
             panel1.BorderStyle = BorderStyle.Fixed3D;
             panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(pictureBox6);
             panel1.Controls.Add(pictureBox5);
             panel1.Controls.Add(pictureBox3);
-            panel1.Controls.Add(button1);
-            panel1.Controls.Add(button2);
+            panel1.Controls.Add(btnExit);
+            panel1.Controls.Add(btnAttendance);
             panel1.Controls.Add(btnDashboard);
+            panel1.Controls.Add(btnProfile);
             panel1.Controls.Add(panel5);
             panel1.Controls.Add(panel3);
             panel1.Controls.Add(panel2);
@@ -74,15 +81,142 @@
             panel1.Size = new Size(263, 720);
             panel1.TabIndex = 0;
             // 
+            // pictureBox4
+            // 
+            pictureBox4.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(0, 236);
+            pictureBox4.Margin = new Padding(0);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(44, 47);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(1, 342);
+            pictureBox6.Margin = new Padding(0);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(44, 47);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(1, 289);
+            pictureBox5.Margin = new Padding(0);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(44, 47);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 5;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(1, 183);
+            pictureBox3.Margin = new Padding(0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(44, 47);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = Color.Indigo;
+            btnExit.FlatAppearance.BorderSize = 0;
+            btnExit.FlatStyle = FlatStyle.Popup;
+            btnExit.ForeColor = Color.White;
+            btnExit.Location = new Point(46, 342);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(215, 47);
+            btnExit.TabIndex = 4;
+            btnExit.Text = "Exit";
+            btnExit.TextAlign = ContentAlignment.MiddleLeft;
+            btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnAttendance
+            // 
+            btnAttendance.BackColor = Color.Indigo;
+            btnAttendance.FlatAppearance.BorderSize = 0;
+            btnAttendance.FlatStyle = FlatStyle.Popup;
+            btnAttendance.ForeColor = Color.White;
+            btnAttendance.Location = new Point(48, 289);
+            btnAttendance.Name = "btnAttendance";
+            btnAttendance.Size = new Size(215, 47);
+            btnAttendance.TabIndex = 4;
+            btnAttendance.Text = "Attendance";
+            btnAttendance.TextAlign = ContentAlignment.MiddleLeft;
+            btnAttendance.UseVisualStyleBackColor = false;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.BackColor = Color.Indigo;
+            btnDashboard.FlatAppearance.BorderSize = 0;
+            btnDashboard.FlatStyle = FlatStyle.Popup;
+            btnDashboard.ForeColor = Color.White;
+            btnDashboard.Location = new Point(48, 183);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Size = new Size(211, 47);
+            btnDashboard.TabIndex = 4;
+            btnDashboard.Text = "Dashboard";
+            btnDashboard.TextAlign = ContentAlignment.MiddleLeft;
+            btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // btnProfile
+            // 
+            btnProfile.BackColor = Color.Indigo;
+            btnProfile.FlatAppearance.BorderSize = 0;
+            btnProfile.FlatStyle = FlatStyle.Popup;
+            btnProfile.ForeColor = Color.White;
+            btnProfile.Location = new Point(47, 236);
+            btnProfile.Name = "btnProfile";
+            btnProfile.Size = new Size(212, 47);
+            btnProfile.TabIndex = 4;
+            btnProfile.Text = "Profile";
+            btnProfile.TextAlign = ContentAlignment.MiddleLeft;
+            btnProfile.UseVisualStyleBackColor = false;
+            // 
             // panel5
             // 
-            panel5.Controls.Add(textBox1);
+            panel5.Controls.Add(label3);
+            panel5.Controls.Add(label1);
             panel5.Controls.Add(pictureBox1);
             panel5.Dock = DockStyle.Top;
             panel5.Location = new Point(0, 0);
             panel5.Name = "panel5";
             panel5.Size = new Size(259, 167);
             panel5.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BorderStyle = BorderStyle.Fixed3D;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(83, 104);
+            label3.Name = "label3";
+            label3.Size = new Size(78, 25);
+            label3.TabIndex = 1;
+            label3.Text = "Center";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(29, 67);
+            label1.Name = "label1";
+            label1.Size = new Size(196, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Ascend Education ";
             // 
             // pictureBox1
             // 
@@ -113,12 +247,35 @@
             // panel4
             // 
             panel4.BackColor = Color.Indigo;
-            panel4.Controls.Add(textBox2);
-            panel4.Controls.Add(textwelcome);
-            panel4.Location = new Point(257, 75);
+            panel4.Controls.Add(textBox1);
+            panel4.Controls.Add(label4);
+            panel4.Controls.Add(label2);
+            panel4.Location = new Point(259, 75);
             panel4.Name = "panel4";
             panel4.Size = new Size(942, 92);
             panel4.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(208, 31);
+            label4.Name = "label4";
+            label4.Size = new Size(51, 34);
+            label4.TabIndex = 3;
+            label4.Text = "{?}";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(49, 31);
+            label2.Name = "label2";
+            label2.Size = new Size(153, 34);
+            label2.TabIndex = 2;
+            label2.Text = "Welcome:";
             // 
             // pictureBox2
             // 
@@ -131,123 +288,18 @@
             pictureBox2.TabIndex = 0;
             pictureBox2.TabStop = false;
             // 
-            // textwelcome
-            // 
-            textwelcome.BackColor = Color.Indigo;
-            textwelcome.ForeColor = Color.White;
-            textwelcome.Location = new Point(149, 28);
-            textwelcome.Multiline = true;
-            textwelcome.Name = "textwelcome";
-            textwelcome.Size = new Size(38, 35);
-            textwelcome.TabIndex = 1;
-            textwelcome.Text = "{?}";
-            textwelcome.TextChanged += textBox1_TextChanged;
-            // 
             // textBox1
             // 
-            textBox1.BackColor = Color.Indigo;
-            textBox1.Dock = DockStyle.Bottom;
-            textBox1.ForeColor = Color.White;
-            textBox1.Location = new Point(0, 94);
-            textBox1.Multiline = true;
+            textBox1.Font = new Font("Calibri", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(658, 31);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(259, 73);
-            textBox1.TabIndex = 1;
-            textBox1.Text = "Ascend Education Center";
-            textBox1.TextAlign = HorizontalAlignment.Center;
-            // 
-            // textBox2
-            // 
-            textBox2.BackColor = Color.Indigo;
-            textBox2.ForeColor = Color.White;
-            textBox2.Location = new Point(28, 28);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(115, 35);
-            textBox2.TabIndex = 1;
-            textBox2.Text = "Welcome:";
-            textBox2.TextChanged += textBox1_TextChanged;
-            // 
-            // btnDashboard
-            // 
-            btnDashboard.BackColor = Color.Indigo;
-            btnDashboard.FlatAppearance.BorderSize = 0;
-            btnDashboard.FlatStyle = FlatStyle.Popup;
-            btnDashboard.ForeColor = Color.White;
-            btnDashboard.Location = new Point(44, 182);
-            btnDashboard.Name = "btnDashboard";
-            btnDashboard.Size = new Size(212, 47);
-            btnDashboard.TabIndex = 4;
-            btnDashboard.Text = "Profile";
-            btnDashboard.TextAlign = ContentAlignment.MiddleLeft;
-            btnDashboard.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(0, 235);
-            pictureBox3.Margin = new Padding(0);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(44, 47);
-            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox3.TabIndex = 5;
-            pictureBox3.TabStop = false;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.Indigo;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Popup;
-            button2.ForeColor = Color.White;
-            button2.Location = new Point(44, 235);
-            button2.Name = "button2";
-            button2.Size = new Size(215, 47);
-            button2.TabIndex = 4;
-            button2.Text = "Dashboard";
-            button2.TextAlign = ContentAlignment.MiddleLeft;
-            button2.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
-            pictureBox4.Location = new Point(1, 182);
-            pictureBox4.Margin = new Padding(0);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(44, 47);
-            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox4.TabIndex = 5;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            pictureBox5.BorderStyle = BorderStyle.Fixed3D;
-            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
-            pictureBox5.Location = new Point(1, 289);
-            pictureBox5.Margin = new Padding(0);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(44, 47);
-            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox5.TabIndex = 5;
-            pictureBox5.TabStop = false;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.Indigo;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Popup;
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(48, 289);
-            button1.Name = "button1";
-            button1.Size = new Size(215, 47);
-            button1.TabIndex = 4;
-            button1.Text = "Log Out";
-            button1.TextAlign = ContentAlignment.MiddleLeft;
-            button1.UseVisualStyleBackColor = false;
+            textBox1.PlaceholderText = "Search for student details";
+            textBox1.Size = new Size(271, 32);
+            textBox1.TabIndex = 4;
             // 
             // Form3
             // 
+            AccessibleRole = AccessibleRole.Window;
             AutoScaleDimensions = new SizeF(12F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
@@ -263,15 +315,16 @@
             Text = "Form3";
             WindowState = FormWindowState.Minimized;
             panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             panel5.ResumeLayout(false);
             panel5.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel4.ResumeLayout(false);
             panel4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
             ResumeLayout(false);
         }
 
@@ -284,14 +337,18 @@
         private Panel panel5;
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
-        private TextBox textwelcome;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Button btnDashboard;
+        private Button btnProfile;
         private PictureBox pictureBox3;
         private PictureBox pictureBox4;
-        private Button button2;
+        private Button btnDashboard;
         private PictureBox pictureBox5;
-        private Button button1;
+        private Button btnAttendance;
+        private PictureBox pictureBox6;
+        private Button btnExit;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
     }
 }
