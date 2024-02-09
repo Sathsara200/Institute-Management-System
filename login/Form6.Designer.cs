3988d30b7451d85b0fc3e462e320f29894c1e05a
Yuvithsanilka <uvithshanilka@gmail.com>
2024-02-09 11:55:59 +0530

    Attendeace form Dising

---
+++ login/Form6.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace login
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            Name = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            textBox6 = new TextBox();
+            button1 = new Button();
+            button3 = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            label6 = new Label();
+            button4 = new Button();
+            label7 = new Label();
+            textBox7 = new TextBox();
+            label8 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Science", "Mathe", "ICT", "Business", "Physice", "Econ", "Accounts", "Sft", "Et" });
+            comboBox1.Location = new Point(64, 65);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(193, 28);
+            comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "12", "13" });
+            comboBox2.Location = new Point(281, 66);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(193, 28);
+            comboBox2.TabIndex = 1;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(535, 147);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(194, 27);
+            dateTimePicker1.TabIndex = 2;
+            // 
+            // Name
+            // 
+            Name.AutoSize = true;
+            Name.ForeColor = SystemColors.Window;
+            Name.Location = new Point(64, 118);
+            Name.Name = "Name";
+            Name.Size = new Size(49, 20);
+            Name.TabIndex = 3;
+            Name.Text = "Neme";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(145, 118);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(194, 27);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.Window;
+            label1.Location = new Point(64, 158);
+            label1.Name = "label1";
+            label1.Size = new Size(24, 20);
+            label1.TabIndex = 3;
+            label1.Text = "ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.Window;
+            label2.Location = new Point(64, 197);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Subject ID";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(145, 154);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(194, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(145, 190);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(194, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.Window;
+            label3.Location = new Point(445, 114);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Student ID";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.Window;
+            label4.Location = new Point(445, 157);
+            label4.Name = "label4";
+            label4.Size = new Size(41, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Date";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = SystemColors.Window;
+            label5.Location = new Point(445, 190);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 20);
+            label5.TabIndex = 3;
+            label5.Text = "Grade";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(535, 111);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(194, 27);
+            textBox4.TabIndex = 4;
+            textBox4.TextChanged += this.textBox4_TextChanged;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(535, 184);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(194, 27);
+            textBox6.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(435, 269);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Insert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(535, 269);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 7;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 304);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(776, 134);
+            dataGridView1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(635, 64);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 9;
+            button2.Text = "Search";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(23, 187);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 20);
+            label6.TabIndex = 3;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(635, 269);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 7;
+            button4.Text = "Delete";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = SystemColors.Window;
+            label7.Location = new Point(64, 237);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 20);
+            label7.TabIndex = 3;
+            label7.Text = "Status";
+            label7.Click += label7_Click;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(145, 230);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(194, 27);
+            textBox7.TabIndex = 4;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = SystemColors.Window;
+            label8.Location = new Point(265, 9);
+            label8.Name = "label8";
+            label8.Size = new Size(276, 38);
+            label8.TabIndex = 10;
+            label8.Text = "Student Attendeace";
+            // 
+            // Form6
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Indigo;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label8);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox7);
+            Controls.Add(textBox4);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(label7);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(Name);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "Form6";
+            Text = "Form6";
+            Load += Form6_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private DateTimePicker dateTimePicker1;
+        private Label Name;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox4;
+        private TextBox textBox6;
+        private Button button1;
+        private Button button3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Label label6;
+        private Button button4;
+        private Label label7;
+        private TextBox textBox7;
+        private Label label8;
+    }
+}
