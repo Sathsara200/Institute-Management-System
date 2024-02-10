170331f307881e26e66dd0aa848ff0dd0ea4ab98
Yuvithsanilka <uvithshanilka@gmail.com>
2024-02-10 15:43:52 +0530

    Payment form

---
+++ login/Form6.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace login
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(329, 79);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Subject";
+            textBox1.Size = new Size(318, 27);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(364, 303);
+            button1.Name = "button1";
+            button1.Size = new Size(69, 32);
+            button1.TabIndex = 1;
+            button1.Text = "Insert";
+            button1.TextImageRelation = TextImageRelation.ImageAboveText;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(59, 82);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(264, 27);
+            dateTimePicker1.TabIndex = 2;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(59, 131);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Name";
+            textBox2.Size = new Size(226, 27);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(59, 243);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Student Id";
+            textBox3.Size = new Size(222, 27);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(59, 192);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Status";
+            textBox4.Size = new Size(222, 27);
+            textBox4.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(676, 79);
+            button2.Name = "button2";
+            button2.Size = new Size(69, 32);
+            button2.TabIndex = 1;
+            button2.Text = "Search";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(439, 303);
+            button3.Name = "button3";
+            button3.Size = new Size(69, 32);
+            button3.TabIndex = 1;
+            button3.Text = "Update";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button1_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(514, 303);
+            button4.Name = "button4";
+            button4.Size = new Size(69, 32);
+            button4.TabIndex = 1;
+            button4.Text = "Delete";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button1_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(589, 303);
+            button5.Name = "button5";
+            button5.Size = new Size(69, 32);
+            button5.TabIndex = 1;
+            button5.Text = "Clear";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Window;
+            label1.Location = new Point(309, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 41);
+            label1.TabIndex = 6;
+            label1.Text = "Payment";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // Form6
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Indigo;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form6";
+            Text = "Form6";
+            Load += Form6_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label1;
+    }
+}
---
+++ login/Form6.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
