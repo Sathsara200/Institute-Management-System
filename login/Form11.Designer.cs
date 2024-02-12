3a17ea8eef248cbb360c60c6c0d10d630f710d21
Yuvithsanilka <uvithshanilka@gmail.com>
2024-02-12 19:21:50 +0530

    Fee Details Form Desing

---
+++ login/Form11.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace login
+{
+    partial class Form11
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            dateTimePicker3 = new DateTimePicker();
+            textBox7 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(23, 125);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(170, 27);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Window;
+            label1.Location = new Point(23, 178);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Fee ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Window;
+            label2.Location = new Point(23, 213);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Student ID";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.Window;
+            label3.Location = new Point(23, 244);
+            label3.Name = "label3";
+            label3.Size = new Size(109, 28);
+            label3.TabIndex = 1;
+            label3.Text = "Subject ID";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.Window;
+            label4.Location = new Point(23, 277);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 28);
+            label4.TabIndex = 1;
+            label4.Text = "Status";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.Window;
+            label5.Location = new Point(384, 179);
+            label5.Name = "label5";
+            label5.Size = new Size(148, 28);
+            label5.TabIndex = 1;
+            label5.Text = "Student Name";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.Window;
+            label6.Location = new Point(384, 213);
+            label6.Name = "label6";
+            label6.Size = new Size(144, 28);
+            label6.TabIndex = 1;
+            label6.Text = "Subject Name";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.Window;
+            label7.Location = new Point(384, 244);
+            label7.Name = "label7";
+            label7.Size = new Size(95, 28);
+            label7.TabIndex = 1;
+            label7.Text = "Fee Date";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(138, 178);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(236, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(138, 211);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(236, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(138, 244);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(236, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(138, 277);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(236, 27);
+            textBox4.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(534, 179);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(236, 27);
+            textBox5.TabIndex = 2;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(534, 212);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(236, 27);
+            textBox6.TabIndex = 2;
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.CustomFormat = "yyyy-MM-dd";
+            dateTimePicker3.Format = DateTimePickerFormat.Custom;
+            dateTimePicker3.Location = new Point(534, 246);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.Size = new Size(236, 27);
+            dateTimePicker3.TabIndex = 0;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(199, 125);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(468, 27);
+            textBox7.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.BlueViolet;
+            button1.Location = new Point(673, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.BlueViolet;
+            button2.Location = new Point(379, 343);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Insert";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.BlueViolet;
+            button3.Location = new Point(479, 343);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "Delete";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.BlueViolet;
+            button4.Location = new Point(579, 343);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 4;
+            button4.Text = "Update";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.BlueViolet;
+            button5.Location = new Point(679, 343);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 4;
+            button5.Text = "Clear";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = SystemColors.Window;
+            label8.Location = new Point(302, 45);
+            label8.Name = "label8";
+            label8.Size = new Size(171, 41);
+            label8.TabIndex = 1;
+            label8.Text = "Fee Details";
+            // 
+            // Form11
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Indigo;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dateTimePicker3);
+            Controls.Add(label8);
+            Controls.Add(label1);
+            Controls.Add(dateTimePicker1);
+            Name = "Form11";
+            Text = "Form11";
+            Load += Form11_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private DateTimePicker dateTimePicker3;
+        private TextBox textBox7;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Label label8;
+    }
+}
